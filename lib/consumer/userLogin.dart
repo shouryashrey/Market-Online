6582bcf9241489a67e52ee1176e2c4a6e2b0443d
shouryashrey <51378838+shouryashrey@users.noreply.github.com>
2020-10-27 10:33:13 +0530

    Rearranged

--- lib/consumer/userLogin.dart
+++ lib/consumer/userLogin.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:market_online/consumer/userSignup.dart';
-import 'package:market_online/homeScreen.dart';
+import 'package:market_online/home/homeScreen.dart';
 
 
 class userLogin extends StatefulWidget {
--- lib/consumer/userSignup.dart
+++ lib/consumer/userSignup.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import '../homeScreen.dart';
+import '../home/homeScreen.dart';
 
 class userSignup extends StatefulWidget {
   @override
