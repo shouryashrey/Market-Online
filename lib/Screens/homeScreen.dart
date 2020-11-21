5e497d8bde2bda7eea327f14c2181c94a5f20324
ANSH GUPTA <68772910+PROFESSORRQ@users.noreply.github.com>
2020-11-21 22:38:40 +0530

    UPDATED homescreen.dart
    Added QR code scanner

--- lib/Screens/homeScreen.dart
+++ lib/Screens/homeScreen.dart
@@ -1,13 +1,60 @@
 import 'package:flutter/material.dart';
-
-class homeScreen extends StatefulWidget {
-  @override
-  _homeScreenState createState() => _homeScreenState();
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+void main(List<String> args) {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home:HomeScreen(),
+  ));
 }
 
-class _homeScreenState extends State<homeScreen> {
+class HomeScreen extends StatefulWidget{
+  @override
+  HomeScreenState createState() => HomeScreenState();
+}
+
+class HomeScreenState extends State<HomeScreen> {
+String result="hey there !";
+Future _scanQR() async{
+  try{
+    String qrResult = await FlutterBarcodeScanner.scanBarcode("#000000","Cancel", true, ScanMode.QR);
+    setState(() {
+      result=qrResult;
+    });
+  }
+  on FormatException{
+setState(() {
+    result="You pressed the back button before scanning";
+     });
+  }catch (ex){
+setState(() {
+      result="Unknown Error $ex";
+   });  
+  }
+  // }
+  // } on FormatException{
+  //   
+  // }catch (ex){
+  //   
+  // }
+}
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ARZI"),
+      ),
+      body: Center(
+        child: Text(
+          result,
+          style: new TextStyle(fontSize:30.0,fontWeight: FontWeight.bold),
+          ),
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        icon:Icon(Icons.camera_alt_rounded),
+        label: Text("Scan"),
+        onPressed: _scanQR,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
